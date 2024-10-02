b11119cd519d4408c83452b7363e55c23e8bee89
hkcodebase <hkchauhan022@gmail.com>
2024-10-01 20:44:32 -0400

    create account form udpdated

--- src/main/frontend/src/components/AccountCreate.tsx
+++ src/main/frontend/src/components/AccountCreate.tsx
@@ -1,6 +1,6 @@
 import {ChangeEvent, FormEvent, useState} from "react";
 import {Account} from "./Account.tsx";
-import {Box, Button, TextField} from "@mui/material";
+import {Box, Button, InputLabel, NativeSelect, TextField} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 export function AccountCreate() {
@@ -38,92 +38,81 @@ export function AccountCreate() {
 
     //const [open, setOpen] = useState(true);
 
-    const style = {
-        width: 'auto',
-        paddingLeft: '10px',
-        paddingRight:'10px',
-    };
 
 
     return (
                     <Box
                         component="form"
-                        sx={style}
                         noValidate
                         autoComplete="off"
                         onSubmit={(e) => handleSubmit(e)}>
                         <div className="div-margin">
                             <h4 className="align-center">Create Account</h4>
-                            <br/>
-                            <table>
-                                <tbody>
-                                <tr>
-                                    <td>
-                                        Name:&nbsp;&nbsp;
-                                    </td>
-                                    <td>
-                                        <TextField
-                                            required
-                                            id="outlined-required"
-                                            // label="Required"
-                                            name={"name"}
-                                            defaultValue={selectedAccount.name}
-                                            value={selectedAccount.name}
-                                            onChange={handleChange}
-                                        />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Total Value:&nbsp;&nbsp;
-                                    </td>
-                                    <td>
-                                        <TextField
-                                            required
-                                            id="outlined-required"
-                                            // label="Required"
-                                            type="number"
-                                            name={"totalValue"}
-                                            defaultValue={selectedAccount.totalValue}
-                                            value={selectedAccount.totalValue}
-                                            onChange={handleChange}
-                                            slotProps={{
-                                                inputLabel: {
-                                                    shrink: true,
-                                                },
-                                            }}
-                                        />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Currency:&nbsp;&nbsp;
-                                    </td>
-                                    <td>
-                                        <select onChange={handleSelectChange} name={"currency"} defaultValue={"USD"}>
-                                            <option value={"USD"}>USD</option>
-                                            <option value={"INR"}>INR</option>
-                                        </select>
 
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Status:&nbsp;&nbsp;
-                                    </td>
-                                    <td>
-                                        <select onChange={handleSelectChange} name={"status"} defaultValue={"Active"}>
-                                            <option value={"Active"}>Active</option>
-                                            <option value={"InActive"}>InActive</option>
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><Button type={"submit"} variant="contained"> Save </Button></td>
-                                    <td><Button onClick={() => handleCancel()} variant="outlined"> Cancel </Button></td>
-                                </tr>
-                                </tbody>
-                            </table>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Name"
+                                name={"name"}
+                                defaultValue={selectedAccount.name}
+                                value={selectedAccount.name}
+                                onChange={handleChange}
+                                slotProps={{
+                                    inputLabel: {
+                                        shrink: true,
+                                    },
+                                }}
+                            />
+                            <br/>
+                            <br/>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Total Value"
+                                type="number"
+                                name={"totalValue"}
+                                defaultValue={selectedAccount.totalValue}
+                                value={selectedAccount.totalValue}
+                                onChange={handleChange}
+                                slotProps={{
+                                    inputLabel: {
+                                        shrink: true,
+                                    },
+                                }}
+                            />
+                            <br/>
+                            <br/>
+                            <InputLabel variant="standard" htmlFor="currency-native">
+                                Currency
+                            </InputLabel>
+                            <NativeSelect
+                                name={"currency"}
+                                defaultValue={selectedAccount.currency}
+                                value={selectedAccount.currency}
+                                onChange={handleSelectChange}
+                            >
+                                <option value={"USD"}>USD</option>
+                                <option value={"INR"}>INR</option>
+                            </NativeSelect>
+                            <br/>
+                            <br/>
+                            <InputLabel variant="standard" htmlFor="status-native">
+                                Status
+                            </InputLabel>
+                            <NativeSelect
+                                defaultValue={"Active"}
+                                name={"status"}
+                                value={selectedAccount.status}
+                                onChange={handleSelectChange}
+                            >
+                                <option value={"Active"}>Active</option>
+                                <option value={"InActive"}>InActive</option>
+                            </NativeSelect>
+                            <br/>
+                            <br/>
+                            <Button type={"submit"} variant="contained"> Save </Button> &nbsp;
+                            <Button onClick={() => handleCancel()} variant="outlined"> Cancel </Button>
+
                         </div>
                     </Box>
     )
